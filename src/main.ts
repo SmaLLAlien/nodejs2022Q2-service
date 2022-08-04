e72127c53641cc9516b3d775f45c477352577d3b
Vladyslav Bilousov <vladyslav_bilousov@epam.com>
2022-08-04 18:23:34 +0300

    feat: swagger added

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 import { CustomLoggerService } from './logger/services/custom-logger.service';
 import { HttpExceptionFilter } from './filters/http-exception-filter';
+import { dirname, join } from 'path';
+import { parse } from 'yaml';
+import { readFile } from 'fs/promises';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger('main');
@@ -24,6 +28,12 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const PORT = configService.get<number>('PORT');
+
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(PORT);
 
   console.log('************************************************************\n');
